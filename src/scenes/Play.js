6527f4df0a5f595c1053bc9741b5543572522b52
Moises Perez <perezmoises132@gmail.com>
2021-04-07 14:26:22 -0700

    Starfield and borders

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,9 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
     create() {
+        this.starfield = this.add.tileSprite(
+            0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
         //green ui background
         this.add.rectangle(0, borderUISize + borderPadding, 
             game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
@@ -13,5 +16,9 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-        }
+    
+    }
+    update() {
+        this.starfield.x += 4;
+    }
 }
